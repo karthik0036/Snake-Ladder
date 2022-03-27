c5c6d2f1a26bdfa5a654c5716581918b3d841c50
Karthik <jeefkarthik036@gmail.com>
2022-03-27 21:36:31 +0530

    sixth-edited UC4

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -8,35 +8,29 @@ public class SnakeAndLadder {
     public static final int WINNING_Position = 100;
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		int player1Pos = 0;
-		System.out.println("Game begins:Player1 starts at position: "+ player1Pos);
-		
-		while (player1Pos<=100) {
-		int rollDice= (int) (Math.floor(Math.random() * 10) % 6 )+1;
-		System.out.println("value of rolled Dice: "+ rollDice);
-		
-		// 0,1,2
-		
-		int opt = (int) Math.floor(Math.random() * 10) % 3;
-	        switch (opt) {
-	            case NO_PLAY:
-	                System.out.println("Player1 stays in same position: "+player1Pos);
-	                break;
-	            case LADDER:
-	                player1Pos = player1Pos + rollDice;
-	                System.out.println("Player1 moves ahead by: "+player1Pos);
-	                break;
-	            case SNAKE:
-	                player1Pos = player1Pos - rollDice;
-	                if(player1Pos < 0){
-	                    player1Pos = 0;
-	                }
-	                System.out.println("Player1 moves back: "+player1Pos);
-	                break;
-	        }
-	        System.out.println("Player1 won: "+player1Pos);
-}
+        System.out.println("Game begins: player1 at position: " + player1Pos);
+        while (player1Pos<=100) {
+            int rollDice = (int) (Math.floor(Math.random() * 10) % 6) + 1;
+            int opt = (int) Math.floor(Math.random() * 10) % 3;
+            switch (opt) {
+                case NO_PLAY:
+                    System.out.println("Player1 stays in same position: " + player1Pos);
+                    break;
+                case LADDER:
+                    player1Pos = player1Pos+ rollDice;
+                    System.out.println("Ladder "+rollDice+" Player1 position: " + player1Pos);
+                    break;
+                case SNAKE:
+                    player1Pos = player1Pos - rollDice;
+                    if (player1Pos < 0) {
+                        player1Pos = 0;
+                    }
+                    System.out.println("Snake "+rollDice+" Player1 position: " + player1Pos);
+                    break;
+            }
+        }
+        System.out.println("Player1 won: "+player1Pos);
 }
 }
 
