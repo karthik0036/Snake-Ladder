43e190b49248a0da254cdb8970ebe5325f35fcf2
Karthik <jeefkarthik036@gmail.com>
2022-03-27 19:00:18 +0530

    second-added UC1

---
+++ SnakeAndLadder.java
@@ -0,0 +1,13 @@
+package com.bridge;
+
+public class SnakeAndLadder {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int player1Pos = 0;
+		System.out.println("Game begins:Player1 starts at position: "+ player1Pos);
+		
+
+	}
+
+}
